91e3be1d4f9fc07b01cd8209d887397679767457
calvinalvito <calvinalvito19@gmail.com>
2022-06-27 22:01:36 +0700

    CORS

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const adminRouter = require('./routes/admin');
 const apiRouter = require('./routes/api');
-app.use(cors());
 var app = express();
 
 // view engine setup
@@ -46,6 +45,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter);
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
