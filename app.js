6c8044db92de7a7795443f970bf2d5c2334d080b
calvinalvito <calvinalvito19@gmail.com>
2022-06-27 22:18:13 +0700

    cors

--- app.js
+++ app.js
@@ -46,6 +46,23 @@ app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter);
 app.use(cors());
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+//Cors Configuration - End
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
